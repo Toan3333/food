5d7fb5e5b23cf7b8a5666b72d77aafc5194a6a99
Anh Toan <toan21420@gmail.com>
2024-07-21 16:38:02 +0700

    Upload source code

--- foodz-client/vite.config.js
+++ foodz-client/vite.config.js
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/food/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    mimeTypes: {
+      ".css": "text/css",
+    },
+  },
 });
