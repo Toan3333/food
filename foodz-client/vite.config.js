0b2f170ad342033e2c8a78d647f9f0843a80e3a5
Anh Toan <toan21420@gmail.com>
2024-07-21 20:08:45 +0700

    Upload source code

--- foodz-client/vite.config.js
+++ foodz-client/vite.config.js
@@ -5,7 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: "/food/",
+
   build: {
+    outDir: "dist", // Thư mục output là 'dist'
     chunkSizeWarningLimit: 1000, // tăng giới hạn lên 1000 KB
   },
 });
